92506f55a399c214c8e7c513a58fbe3fb174a5be
AKR-2803 <akr28032001@gmail.com>
2024-12-16 19:47:18 -0700

    POTD 12-16-2024 done

---
+++ POTD/December/code/FinalArrayStateAfterK.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] getFinalState(int[] nums, int k, int multiplier) {
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> {
+            if(a[0] != b[0]){
+                return a[0] - b[0]; // when differnt values, go for lesser one
+            } else{
+                return a[1] - b[1]; // when same value, go for lesser index (value appearing first)
+            }   
+        });
+
+        for(int i = 0; i < nums.length; i++){
+            heap.offer(new int[]{ nums[i], i});
+        }
+
+        // take min element each time for `k` times
+        for(int i = 0; i < k; i++){
+            int[] pairMin = heap.poll();
+            int idx = pairMin[1];
+
+            nums[idx] *= multiplier;
+            
+            heap.offer(new int[]{nums[idx], idx});
+        }
+
+        return nums;
+    }
+}
